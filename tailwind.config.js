2fb29c818b1fabfc739d85c706ebd8d541c01481
JoelVStan <joelvstan@gmail.com>
2023-08-31 14:56:22 +0530

    Website ready

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,28 @@ module.exports = {
     fontFamily: {
       Poppins: ["Poppins", "sans-serif"]
     },
+    screens: {
+      'xl': {'max': '1200px'},
+      'lg': {'max': '991px'},
+      'md': {'max': '768px'},
+      'sm': {'max': '617px'},
+      'xs': {'max': '450px'},
+    },
     extend: {
       colors: {
         "bg-color": "var(--bg-color)",
-        "second-bg-color": "var(--secondbg-color)",
+        "second-bg-color": "var(--second-bg-color)",
         "text-color": "var(--text-color)",
         "main-color": "var(--main-color)",
+      },
+      keyframes: {
+        floatImage: {
+          '0%, 100%': { transform: 'rotate(-2deg)' },
+          '50%': { transform: 'rotate(2deg)' },
+        }
+      },
+      animation: {
+        floatImage: 'floatImage 2s ease-in-out infinite'
       }
     },
   },
